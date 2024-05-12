dc58f98f75f75155c2c90aede027662576ac3547
luisduran98 <104513661+luisduran98@users.noreply.github.com>
2024-05-11 23:55:10 -0300

    actualizacion

--- Calculadora/Exercise_Calculadora.py
+++ Calculadora/Exercise_Calculadora.py
@@ -1,4 +1,81 @@
-from tkinter.messagebox import showinfo as alert
+from os import system
+
+
+
+def suma (a,b):
+    return int(a + b) 
+    
+
+def menu():
+    system("cls")
+    print("1. Ingresar 1er operando: ")
+    print("2. Ingresar 2do operando: ")
+    print("3. Sumar")
+    print("4. Restar")
+    print("5. Dividir")
+    print("6. Multiplicar")
+    print("7. Factorial")
+    print("8. Salir")
+    return input("Introduce una opcion :")
+
+flag_primer_operando = False
+flag_segundo_operando = False
+
+while True:
+    match menu():
+
+        case "1":
+            caso_uno = input("Coloca tu primer numero: ")
+            while not caso_uno.isdigit():
+                system("cls")
+                print("Debe ser un numero natural")
+                caso_uno = input("Intenta de nuevo: ")
+    
+            flag_primer_operando = True
+            primer_operando = int(caso_uno)
+           
+        case "2":
+            if flag_primer_operando:
+                caso_dos = input("Coloca tu segundo numero: ")
+                while not caso_dos.isdigit():
+                    system("cls")
+                    print("Debe ser un numero natural")
+                    caso_dos = input("Intenta de nuevo: ")
+    
+                flag_segundo_operando = True
+                segundo_operando = int(caso_dos)
+            else:
+                system("cls")
+                print("Deberias colocar el primer operando")
+                system("pause")
+        
+        case "3":
+            if flag_primer_operando and flag_primer_operando:
+                valor_suma = suma(primer_operando, segundo_operando)
+            # if not flag_primer_operando:
+
+
+        # case "4":
+
+        # case "5":
+
+        # case "6":
+
+        # case "7":
+
+        # case "8":
+
+
+# def resta(a,b):
+#     return int(a - b)
+
+# def division(a,b):
+#     try:
+#          print(int(a/b))
+#     except:
+#         alert("Error","No se puede dividir por cero")
+
+
 
 # Calculadora / video hora = 1:00:00
 
@@ -32,27 +109,3 @@ from tkinter.messagebox import showinfo as alert
 # • Deberán contemplarse los casos de error (división por cero, etc.)
 
 # • Documentar todas las funciones
-
-def suma (a,b):
-    return int(a + b) 
-
-def resta(a,b):
-    return int(a - b)
-
-def division(a,b):
-    try:
-         print(int(a/b))
-    except:
-        alert("Error","No se puede dividir por cero")
-    
-
-
-# a = input("Ingresar 1er operando: ")
-# while not a.isdigit():
-#     a = input("Ingresar 1er operando: ")
-
-# b = input("Ingresar 2do operando: ")
-# while not b.isdigit():
-#     b = input("Ingresar 2do operando: ")
-
- 
