b398748c10355777ccb6953eac5e8682c2d783b6
luisduran98 <104513661+luisduran98@users.noreply.github.com>
2024-05-12 02:09:31 -0300

    actualizacion

--- Calculadora/Exercise_Calculadora.py
+++ Calculadora/Exercise_Calculadora.py
@@ -1,81 +1,4 @@
-from os import system
-
-
-
-def suma (a,b):
-    return int(a + b) 
-    
-
-def menu():
-    system("cls")
-    print("1. Ingresar 1er operando: ")
-    print("2. Ingresar 2do operando: ")
-    print("3. Sumar")
-    print("4. Restar")
-    print("5. Dividir")
-    print("6. Multiplicar")
-    print("7. Factorial")
-    print("8. Salir")
-    return input("Introduce una opcion :")
-
-flag_primer_operando = False
-flag_segundo_operando = False
-
-while True:
-    match menu():
-
-        case "1":
-            caso_uno = input("Coloca tu primer numero: ")
-            while not caso_uno.isdigit():
-                system("cls")
-                print("Debe ser un numero natural")
-                caso_uno = input("Intenta de nuevo: ")
-    
-            flag_primer_operando = True
-            primer_operando = int(caso_uno)
-           
-        case "2":
-            if flag_primer_operando:
-                caso_dos = input("Coloca tu segundo numero: ")
-                while not caso_dos.isdigit():
-                    system("cls")
-                    print("Debe ser un numero natural")
-                    caso_dos = input("Intenta de nuevo: ")
-    
-                flag_segundo_operando = True
-                segundo_operando = int(caso_dos)
-            else:
-                system("cls")
-                print("Deberias colocar el primer operando")
-                system("pause")
-        
-        case "3":
-            if flag_primer_operando and flag_primer_operando:
-                valor_suma = suma(primer_operando, segundo_operando)
-            # if not flag_primer_operando:
-
-
-        # case "4":
-
-        # case "5":
-
-        # case "6":
-
-        # case "7":
-
-        # case "8":
-
-
-# def resta(a,b):
-#     return int(a - b)
-
-# def division(a,b):
-#     try:
-#          print(int(a/b))
-#     except:
-#         alert("Error","No se puede dividir por cero")
-
-
+from funciones_matematicas.matematica import *
 
 # Calculadora / video hora = 1:00:00
 
@@ -109,3 +32,148 @@ while True:
 # • Deberán contemplarse los casos de error (división por cero, etc.)
 
 # • Documentar todas las funciones
+
+flag_primer_operando = False
+flag_segundo_operando = False
+resultado = 0
+parametro_uno = None
+parametro_dos = None
+
+while True: 
+    match menu(parametro_uno,parametro_dos):
+
+        case "1":
+            caso_uno = input("Coloca tu primer numero: ")
+            while not caso_uno.isdigit():
+                limpiar()
+                print("Debe ser un numero natural")
+                caso_uno = input("Intenta de nuevo: ")
+    
+            flag_primer_operando = True
+            primer_operando = int(caso_uno)
+            parametro_uno = caso_uno
+           
+        case "2":
+            if flag_primer_operando:
+                caso_dos = input("Coloca tu segundo numero: ")
+                while not caso_dos.isdigit():
+                    limpiar()
+                    print("Debe ser un numero natural")
+                    caso_dos = input("Intenta de nuevo: ")
+    
+                flag_segundo_operando = True
+                segundo_operando = int(caso_dos)
+                parametro_dos = caso_dos
+            else:
+                limpiar()
+                print("Deberias colocar el primer operando")
+                pausar()
+        
+        case "3":
+                while True:
+                     match operaciones():
+                          case "1":
+                                if flag_primer_operando and flag_segundo_operando:
+                                    resultado = suma(primer_operando, segundo_operando)
+                                    break
+                                if flag_primer_operando and not flag_segundo_operando:
+                                    limpiar()
+                                    print("Para poder sumar te hace falta el segundo operador")
+                                    pausar()
+                                    break 
+                                else: 
+                                    limpiar()
+                                    print("Para poder sumar te faltan los dos operadores")
+                                    pausar()
+                                    break
+                          case "2":
+                                if flag_primer_operando and flag_segundo_operando:
+                                    resultado = resta(primer_operando, segundo_operando)
+                                    break
+                                if flag_primer_operando and not flag_segundo_operando:
+                                    limpiar()
+                                    print("Para poder restar te hace falta el segundo operador")
+                                    pausar()
+                                    break 
+                                else: 
+                                    limpiar()
+                                    print("Para poder restar te faltan los dos operadores")
+                                    pausar()
+                                    break
+                          case "3":
+                                if flag_primer_operando and flag_segundo_operando:
+                                    try:
+                                        resultado = division(primer_operando, segundo_operando)
+                                        break
+                                    except ZeroDivisionError as error:
+                                        limpiar()
+                                        print(error)
+                                        pausar()                                       
+                                        break
+                                if flag_primer_operando and not flag_segundo_operando:
+                                    limpiar()
+                                    print("Para poder dividir te hace falta el segundo operador")
+                                    pausar()
+                                    break 
+                                else: 
+                                    limpiar()
+                                    print("Para poder dividir te faltan los dos operadores")
+                                    pausar()
+                                    break
+                          case "4":
+                                if flag_primer_operando and flag_segundo_operando:
+                                    resultado = multiplicar(primer_operando, segundo_operando)
+                                    break
+                                if flag_primer_operando and not flag_segundo_operando:
+                                    limpiar()
+                                    print("Para poder multiplicar te hace falta el segundo operador")
+                                    pausar()
+                                    break 
+                                else: 
+                                    limpiar()
+                                    print("Para poder multiplicar te faltan los dos operadores")
+                                    pausar()
+                                    break
+                          case "5":
+                                if flag_primer_operando and flag_segundo_operando:
+                                    valor_factorial_uno = factorial(primer_operando)
+                                    valor_factorial_dos = factorial(segundo_operando)
+                                    break
+                                if flag_primer_operando and not flag_segundo_operando:
+                                    limpiar()
+                                    print("Necesitas el segundo operador para saber cual es el factorial")
+                                    pausar()
+                                    break 
+                                else: 
+                                    limpiar()
+                                    print("Te faltan los dos operadores")
+                                    pausar()
+                                    break
+                          case "6":
+                                break
+        case "4":
+            if flag_primer_operando and flag_segundo_operando:
+                if resultado:
+                    limpiar()
+                    print(resultado)
+                    pausar()
+                    continue
+                if valor_factorial_dos and valor_factorial_uno:
+                    limpiar()
+                    print(f"El Valor de A = {valor_factorial_uno} y el valor de B = {valor_factorial_dos} ")
+                    pausar()
+                    continue
+                else:
+                    limpiar()
+                    print("Necesitas seleccionar alguna operacion para poder mostrar los resultados")
+                    pausar()
+            if flag_primer_operando and not flag_segundo_operando:
+                limpiar()
+                print("Para poder avanzar te hace falta el segundo operador y seleccionar una operacion")
+                pausar()
+            else: 
+                limpiar()
+                print("te faltan los dos operadores y seleccionar una operacion")
+                pausar()
+        case "5":
+            break
